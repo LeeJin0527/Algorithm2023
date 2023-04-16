793209845b88a80151147b44a990997e01d712eb
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-04-16 18:17:58 +0900

    [level 2] Title: 프린터, Time: 0.54 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -1,22 +1,26 @@
 from collections import deque
 def solution(priorities, location):
     answer = []
-    q = deque()
+    valueDeque = deque()
+    totalDeque = deque()
     for index, value in enumerate(priorities):
-        q.append([value, index])
-    while q:
-        temp = []
-        if len(q) == 1:
-            value, index = q.popleft()
+        valueDeque.append(value)
+        totalDeque.append([value, index])
+    print(valueDeque)
+    print(totalDeque)
+    
+    while totalDeque:
+        if len(totalDeque) == 1:
+            value, index = totalDeque.popleft()
             answer.append([value, index])
             break
-        value, index = q.popleft()
-        for each in q:
-            temp.append(each[0])
-        if value >= max(temp):
+        value, index = totalDeque.popleft()
+        checkValue = valueDeque.popleft()
+        if value >= max(valueDeque):
             answer.append([value, index])
         else:
-            q.append([value, index])
+            totalDeque.append([value, index])
+            valueDeque.append(checkValue)
     for index, value in enumerate(answer):
         if value[1] == location:
             return index + 1
