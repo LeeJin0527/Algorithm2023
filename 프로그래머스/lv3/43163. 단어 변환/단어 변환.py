35f19ff5713cf3924c5c8f5b3bfb147d155d9425
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-04-18 13:44:13 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,25 @@
+from collections import deque 
+def can_change(start, end):
+    cnt = 0
+    for index in range(len(start)):
+        if start[index] != end[index]:
+            cnt += 1
+    if cnt == 1:
+        return True
+    return False
+
+def solution(begin, target, words):
+    answer = 0
+    if target not in words:
+        return 0
+    q = deque()
+    q.append([begin, 0])
+    while q:
+        x, cnt = q.popleft()
+        for word in words:
+            if can_change(x, word):
+                if word == target:
+                    return cnt+1
+                else:
+                    q.append([word, cnt+1])
+    return answer
