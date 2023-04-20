10c2c215b09b9f1e4cdb0bc84054358c062d9f71
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-04-20 19:09:48 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 140 ms, Memory: 125884 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -1,31 +1,17 @@
-N = int(input())
-M = int(input())
-
-positions = list(map(int, input().split()))
-len_positions = len(positions)
-
-min_height = 0
-
-# 가로등이 하나 있는 경우
-if len_positions == 1:
-    min_height = max(positions[0] - 0, N - positions[0])
-# 가로등이 하나 이상인 경우
+n = int(input())
+m = int(input())
+lights = list(map(int, input().split()))
+lights.sort()
+if len(lights) == 1:
+    print(max(n - lights[0], lights[0] - 0))
 else:
-    for i in range(len_positions):
-        # 입구 근처의 첫번째 가로등이 비춰줘야할 거리
-        if i == 0:
-            height = positions[i] - 0
-        # 끝나는 지점 기준 가장 가까운 가로등이 비춰줘야할 거리
-        elif i == len_positions - 1:
-            height = N - positions[i]
-        # 양 끝의 가로등을 제외한 가로등
+    first = lights[0] - 0
+    last = n - lights[-1]
+    check = 0
+    for i in range(1, len(lights)-1):
+        tmp = lights[i] - lights[i-1]
+        if tmp % 2 == 0:
+            check = max(check, tmp // 2)
         else:
-            tmp = positions[i] - positions[i-1]
-            if tmp % 2:
-                height = tmp // 2 + 1
-            else:
-                height = tmp // 2 
-        # 가장 큰 범위를 찾으면 되니까 max로 갱신
-        min_height = max(height, min_height)
-
-print(min_height)
+            check = max(tmp // 2 + 1, check)
+    print(max(first, last, check))
