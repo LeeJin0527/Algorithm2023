0c9461655958ce3dcd4d2636b53bbf72e334e348
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-04-25 13:11:35 +0900

    [Silver II] Title: 창고 다각형, Time: 144 ms, Memory: 114844 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -7,23 +7,22 @@ for _ in range(n):
     storages.append([a, b])
 storages.sort()
 for storage in storages:
-	checkHeight.append(storage[1])
+    checkHeight.append(storage[1])
 cur_x, cur_y = storages[0][0], storages[0][1]
 for index in range(len(storages)):
-	if storages[index][0] > cur_x and storages[index][1] >= cur_y:
-		answer += cur_y * (storages[index][0] - cur_x)
-		cur_x = storages[index][0]
-		cur_y = storages[index][1]
-	try:
-		if storages[index][1] > max(checkHeight[index+1:]):
-			cur_x = storages[index][0] + 1
-			cur_y = max(checkHeight[index+1:])
+    if storages[index][0] > cur_x and storages[index][1] >= cur_y:
+        answer += cur_y * (storages[index][0] - cur_x)
+        cur_x = storages[index][0]
+        cur_y = storages[index][1]
+    try:
+        if storages[index][1] > max(checkHeight[index + 1:]):
+            cur_x = storages[index][0] + 1
+            cur_y = max(checkHeight[index + 1:])
 
-			answer += storages[index][1]
-			continue
-	except:
-		pass
-	if index == len(storages)-1:
-		answer += storages[-1][1]
+            answer += storages[index][1]
+    except:
+        pass
+    if index == len(storages) - 1:
+        answer += storages[-1][1]
 
 print(answer)
