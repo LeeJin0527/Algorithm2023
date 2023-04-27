8c12fe9ffd7ee0f1d0b5c5b2b021c5a17697aad8
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-04-27 13:59:52 +0900

    [Silver V] Title: 덩치, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,11 +1,11 @@
 n = int(input())
 lst = []
-for index in range(n):
-    x, y = map(int, input().split())
-    lst.append([index, x, y])
-for l in lst:
-    rank = 1
-    for j in lst:
-        if l[1] < j[1] and l[2] < j[2]:
-            rank += 1
-    print(rank, end = ' ')
+for _ in range(n):
+	a, b = map(int, input().split())
+	lst.append([a, b])
+for i in lst:
+	rank = 1
+	for j in lst:
+		if i[0] < j[0] and i[1] < j[1]:
+			rank += 1
+	print(rank , end = ' ')
