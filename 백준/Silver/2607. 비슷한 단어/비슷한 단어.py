c0f279bd986c179e9acfa0d1202e183448260dfd
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-04-27 19:14:07 +0900

    [Silver III] Title: 비슷한 단어, Time: 132 ms, Memory: 113756 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
@@ -1,22 +1,24 @@
 import copy
-from collections import defaultdict
 n = int(input())
-origin = list(input().rstrip())
-
-cnt = 0
-for _ in range(n-1):
-    checkCopy = copy.deepcopy(origin)
-    compare = list(input().rstrip())
-    if len(compare) > len(checkCopy):
-        for check in checkCopy:
-            if check in compare:
-                compare.remove(check)
+words = []
+for _ in range(n):
+    words.append(list(input().rstrip()))
+origin = words[0]
+words = words[1:]
+answer = 0
+for index in range(len(words)):
+    compare = words[index]
+    if len(compare) >= len(origin):
+        for each in origin:
+            if each in compare:
+                compare.remove(each)
         if len(compare) == 1 or len(compare) == 0:
-            cnt += 1
+            answer += 1
     else:
+        temp = copy.deepcopy(origin)
         for comp in compare:
-            if comp in checkCopy:
-                checkCopy.remove(comp)
-        if len(checkCopy) == 1 or len(checkCopy) == 0:
-            cnt += 1
-print(cnt)
+            if comp in temp:
+                temp.remove(comp)
+        if len(temp) == 1:
+            answer += 1
+print(answer)
