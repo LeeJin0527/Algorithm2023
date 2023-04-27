486afd4a6bfbdd6cdaf413a108f46a765015682b
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-04-27 14:50:45 +0900

    [Silver V] Title: 줄세우기, Time: 248 ms, Memory: 116444 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.py
+++ 백준/Silver/10431. 줄세우기/줄세우기.py
@@ -1,26 +1,23 @@
-import copy
-n = int(input())
-for t in range(1, n+1):
-    lst = list(map(int, input().split()))
-    students = lst[1:]
-    temp = copy.deepcopy(students)
-    check = students.sort()
-    left = [temp[0]]
-    right = temp[1:]
-    checkCnt = 0
-    result = 0
-    if temp == students:
-        print(t, 0)
-        continue
-    for each in right:
-        if each > left[-1]:
-            left.append(each)
-            continue
-        tempCnt = 0
-        for i in range(len(left)):
-            if left[i] > each:
-                break
-            tempCnt += 1
-        result += len(left) - tempCnt
-        left.insert(tempCnt, each)
-    print(t, result)
+from collections import deque
+t = int(input())
+for _ in range(t):
+    answer = 0
+    numbers = list(map(int, input().split()))
+    case = numbers[0]
+    numbers = deque(numbers[1:])
+    sortedStudents = deque()
+    firstStudent = numbers.popleft()
+    sortedStudents.append(firstStudent)
+    while numbers:
+        checkNumber = numbers.popleft()
+        if checkNumber > sortedStudents[-1]:
+            sortedStudents.append(checkNumber)
+        else:
+            index = len(sortedStudents)-1
+            count = 0
+            while sortedStudents[index] > checkNumber and index >= 0:
+                index -= 1
+                count += 1
+            sortedStudents.insert(len(sortedStudents) - count,  checkNumber)
+            answer += count
+    print(f'{case} {answer}')
