eef08d26fb673a6c3e03f7ca3c0e1226b61eeaf0
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-04-28 12:49:51 +0900

    [Silver II] Title: 주식, Time: 1520 ms, Memory: 346568 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,15 +1,12 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
 t = int(input())
 for _ in range(t):
     n = int(input())
-    numbers = list(map(int, input().split()))
-    maxNumber = 0
+    stocks = list(map(int, input().split()))
+    maxValue = stocks[-1]
     answer = 0
-    for index in range(n-1, -1, -1):
-        if numbers[index] > maxNumber:
-            maxNumber = numbers[index]
+    for index in range(len(stocks)-1, -1, -1):
+        if stocks[index] > maxValue:
+            maxValue = stocks[index]
         else:
-            answer += maxNumber - numbers[index]
+            answer += maxValue - stocks[index]
     print(answer)
