cd2701982230169e76db31dd01bbc9e0b1e1c268
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-04-28 17:48:44 +0900

    [Silver I] Title: 지름길, Time: 136 ms, Memory: 115820 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,29 +1,26 @@
 import heapq
-import sys
-input = sys.stdin.readline
-INF = int(1e9)
 n, d = map(int, input().split())
 graph = [[] for _ in range(10001)]
-distance = [INF] * (10001)
+distance = [int(1e9)] * 10001
+
 for _ in range(n):
-    x, y, c = map(int, input().split())
-    graph[x].append([y, c])
-for index in range(d):
+    start, end, cost = map(int, input().split())
+    graph[start].append([end, cost])
+for index in range(d+1):
     graph[index].append([index+1, 1])
 def dijkstra(start):
     q = []
-    heapq.heappush(q, (0, start))
     distance[start] = 0
+    heapq.heappush(q, [0, start])
     while q:
         dist, now = heapq.heappop(q)
-
         if distance[now] < dist:
             continue
-
         for i in graph[now]:
             cost = dist + i[1]
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+                heapq.heappush(q, [cost, i[0]])
+
 dijkstra(0)
 print(distance[d])
