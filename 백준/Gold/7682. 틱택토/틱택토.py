1c87cb98c75ea975131f250b1074bb6604d51771
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-08 19:37:25 +0900

    [Gold V] Title: 틱택토, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.py
+++ 백준/Gold/7682. 틱택토/틱택토.py
@@ -1,84 +1,91 @@
 import sys
 input = sys.stdin.readline
+check = [[(0, 0), [(0, 2), (2, 2), (2, 0) ]],
+         [(0, 1), [(2, 1)]],
+         [(0, 2), [(2, 0), (2, 2)]],
+         [(1, 0), [(1, 2)]],
+         [(2, 0), [(2, 2)]]]
 
-while True:
-    temp = input().rstrip()
-    board = [[] for _ in range(3)]
-    if temp == 'end':
-        break
-    temp = list(temp)
-    for index, tem in enumerate(temp):
-        board[index // 3].append(tem)
-    check = [[(0, 0), [(0, 2), (2, 2), (2, 0)]],
-             [(0, 1), [(2, 1)]],
-             [(0, 2), [(2, 0), (2, 2)]],
-             [(1, 0), [(1, 2)]],
-             [(2, 0), [(2, 2)]]]
-    xCount = 0
-    yCount = 0
+def xoCount(check, pan):
+    xCnt, oCnt = 0, 0
     for che in check:
         start_x, start_y = che[0]
         for each in che[1]:
             end_x, end_y = each
-            if start_x != end_x and start_y == end_y:
-                checkCount = 0
+            if start_x == 0 and start_y == 2 and end_x == 2 and end_y == 0:
+                count = 0
                 for i in range(3):
-                    if board[start_x][start_y] == board[start_x + i][end_y]:
-                        checkCount += 1
-                if checkCount == 3:
-                    if board[start_x][start_y] == 'X':
-                        xCount += 1
-                    elif board[start_x][start_y] == 'O':
-                        yCount += 1
-            elif start_x == end_x and start_y != end_y:
-                checkCount = 0
-                for i in range(3):
-                    if board[start_x][start_y] == board[end_x][start_y + i]:
-                        checkCount += 1
-                if checkCount == 3:
-                    if board[start_x][start_y] == 'X':
-                        xCount += 1
-                    elif board[start_x][start_y] == 'O':
-                        yCount += 1
+                    if pan[start_x][start_y] == pan[start_x + i][start_y - i]:
+                        count += 1
+                if count == 3:
+                    if pan[start_x][start_y] == 'X':
+                        xCnt += 1
+                    elif pan[start_x][start_y] == 'O':
+                        oCnt += 1
 
-            elif start_x != end_x and start_y != end_y:
-                if start_x == 0 and start_y == 2 and end_x == 2:
-                    checkCount = 0
+            elif start_x < end_x:
+                if start_y < end_y:
+                    count = 0
                     for i in range(3):
-                        if board[start_x][start_y] == board[start_x + i][start_y - i]:
-                            checkCount += 1
-                    if checkCount == 3:
-                        if board[start_x][start_y] == 'X':
-                            xCount += 1
-                        elif board[start_x][start_y] == 'O':
-                            yCount += 1
-
-                else:
-                    checkCount = 0
+                        if pan[start_x][start_y] == pan[start_x + i][start_y + i]:
+                            count += 1
+                    if count == 3:
+                        if pan[start_x][start_y] == 'X':
+                            xCnt += 1
+                        elif pan[start_x][start_y] == 'O':
+                            oCnt += 1
+                elif start_y == end_y:
+                    count = 0
                     for i in range(3):
-                        if board[start_x][start_y] == board[start_x + i][start_y + i]:
-                            checkCount += 1
-                    if checkCount == 3:
-                        if board[start_x][start_y] == 'X':
-                            xCount += 1
-                        elif board[start_x][start_y] == 'O':
-                            yCount += 1
+                        if pan[start_x][start_y] == pan[start_x + i][start_y]:
+                            count += 1
+                    if count == 3:
+                        if pan[start_x][start_y] == 'X':
+                            xCnt += 1
+                        elif pan[start_x][start_y] == 'O':
+                            oCnt += 1
+            elif start_x == end_x:
+                if start_y < end_y:
+                    count = 0
+                    for i in range(3):
+                        if pan[start_x][start_y] == pan[start_x][start_y + i]:
+                            count += 1
+                    if count == 3:
+                        if pan[start_x][start_y] == 'X':
+                            xCnt += 1
+                        elif pan[start_x][start_y] == 'O':
+                            oCnt += 1
+    return xCnt, oCnt
+while True:
+    origin = input().rstrip()
+    if origin == 'end':
+        break
+    board = [[] for _ in range(3)]
+    for index, value in enumerate(origin):
+        board[index // 3].append(value)
+    ## X O 몇 빙고 나오는지 세기
+    xCount, oCount = xoCount(check, board)
+    # print(xCount, oCount)
+    ## 빙고 종료 조건
+    # X 승리
     if xCount > 0:
-        if yCount >= 1:
+        if oCount > 0:
             print("invalid")
             continue
-        else:
-            if temp.count('X') == temp.count('O') + 1:
-                print("valid")
-                continue
-    elif yCount > 0:
-        if temp.count('X') == temp.count('O'):
+        elif origin.count('X') == origin.count('O') + 1:
             print("valid")
             continue
-    elif temp.count('X') == 5 and temp.count('O') == 4:
+    # Y 승리
+    if oCount > 0:
+        if origin.count('X') == origin.count('O'):
+            print("valid")
+            continue
+        else:
+            print("invalid")
+            continue
+    # 무승부
+    elif origin.count('X') == 5 and origin.count('O') == 4:
         print("valid")
         continue
-    elif xCount >= 1 and yCount >= 1:
+    else:
         print("invalid")
-        continue
-    print("invalid")
