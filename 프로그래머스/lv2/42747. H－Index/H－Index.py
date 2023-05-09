c9485794570dc1e8acb3dcf590aa005dc5c34347
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-09 22:18:41 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,10 +1,9 @@
 def solution(citations):
-    answer = 0
-    candidate = max(citations)
-    for index in range(candidate, -1, -1):
-        temp = 0
+    start = max(citations)
+    for index in range(start, -1, -1):
+        cnt = 0
         for citation in citations:
             if citation >= index:
-                temp += 1
-        if temp >= index:
+                cnt += 1
+        if cnt >= index:
             return index
