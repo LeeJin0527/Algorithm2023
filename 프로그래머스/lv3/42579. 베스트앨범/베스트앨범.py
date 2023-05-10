c4a142ad19e8126ed32eaee305009a67337efa9b
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-10 11:53:40 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -2,22 +2,23 @@ from collections import defaultdict
 import operator
 def solution(genres, plays):
     answer = []
-    countDic = defaultdict(int)
-    genreDic = defaultdict(list)
-    for index, value in enumerate(genres):
-        countDic[value] += plays[index]
-        genreDic[value].append([plays[index], index])
+    genresCount = defaultdict(int)
+    playsCount = defaultdict(list)
+    for index, genre in enumerate(genres):
+        genresCount[genre] += plays[index]   
+    genresCount = sorted(genresCount.items(), key= operator.itemgetter(1), reverse = True)
     
-    count = sorted(countDic.items(), key=operator.itemgetter(1), reverse = True)
-    for genre in genreDic:
-        genreDic[genre].sort(key=lambda x: [-x[0], x[1]])
+    for index, genre in enumerate(genres):
+        playsCount[genre].append([plays[index], index])
+        
+        
+    for genre in genresCount:
+        temp = sorted(playsCount[genre[0]], key=lambda x: (-x[0], x[1]))
+        if len(temp) >= 2:
+            answer.append(temp[0][1])
+            answer.append(temp[1][1])
+        else:
+            for index in range(len(temp)):
+                answer.append(temp[index][1])
     
-    for cou in count:
-        tempCnt = 0
-        for i in range(len(genreDic[cou[0]])):
-            if tempCnt >= 2:
-                break
-            answer.append(genreDic[cou[0]][i][1])
-            tempCnt += 1
-            
     return answer
