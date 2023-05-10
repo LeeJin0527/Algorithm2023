3b59448a79137f200c90611ae71a210c50356633
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-10 14:10:26 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,17 +1,18 @@
 import math
 def solution(progresses, speeds):
     answer = []
+    works = []
+    stack = []
+    n = len(progresses)
     for index, value in enumerate(progresses):
-        progresses[index] = math.ceil((100 - value) / speeds[index])
-    start = progresses[0]
-    cnt = 0
-    for progress in progresses:
-        if start >= progress:
-            cnt += 1
-        else:
-            answer.append(cnt)
-            start = progress
-            cnt = 1
-    answer.append(cnt)
-    
+        works.append(math.ceil((100 - value) / speeds[index]))
+    index = 0
+    while index < n:
+        check = 1
+        cnt = 1
+        while index + check < n and works[index] >= works[index+check]:
+            check += 1
+        answer.append(check)
+        index += check
+    print(answer)
     return answer
