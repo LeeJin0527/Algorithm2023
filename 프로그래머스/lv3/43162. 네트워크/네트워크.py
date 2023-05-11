925edcd388160db7636dd485e9c9fd93baf7d5fa
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-11 15:13:18 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,15 +1,14 @@
-def dfs(v, visited, graph):
-    visited[v] = True
-    for index in range(len(graph[v])):
-        if not visited[index] and graph[v][index] == 1:
-            visited[index] = True
-            dfs(index, visited, graph)
-                       
+def dfs(value, graph, visited):
+    visited[value] = True
+    for index in range(len(graph[value])):
+        if not visited[index] and graph[value][index] == 1:
+            dfs(index, graph, visited)
+
 def solution(n, computers):
     answer = 0
     visited = [False] * n
     for index in range(n):
         if not visited[index]:
-            dfs(index, visited, computers)
+            dfs(index, computers, visited)
             answer += 1
     return answer
