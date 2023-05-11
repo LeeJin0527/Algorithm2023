a3abe5f2b4163c64ba9077b98730302376e86127
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-11 21:04:39 +0900

    [level 3] Title: 정수 삼각형, Time: 52.98 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,9 +1,11 @@
 def solution(triangle):
-    for index, value in enumerate(triangle):
-        triangle[index] = [0] + triangle[index] + [0]
-    
-    for i in range(1, len(triangle)):
-        for j in range(1, len(triangle[i])-1):
-            triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
-    answer = max(triangle[-1])
-    return answer
+    answer = 0
+    graph = []
+    for tri in triangle:
+        graph.append([0] + tri + [0])
+
+    for index in range(1, len(graph)):
+        for i in range(1, len(graph[index])-1):
+            temp = max(graph[index][i] + graph[index-1][i-1], graph[index][i] + graph[index-1][i])
+            graph[index][i] = temp
+    return max(graph[-1])
