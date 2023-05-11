74e566a7cba197d5b8d84e7d6cbd3c6229b5621f
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-11 12:34:50 +0900

    [level 3] Title: 순위, Time: 205.94 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.py
+++ 프로그래머스/lv3/49191. 순위/순위.py
@@ -1,8 +1,10 @@
 def solution(n, results):
     answer = 0
-    graph = [[-1 for _ in range(n+1)] for _ in range(n+1)]
+    graph = [[-2 for _ in range(n+1)] for _ in range(n+1)]
     for x in range(1, n+1):
-        graph[x][x] = -2
+        for y in range(1, n+1):
+            if x == y:
+                graph[x][y] = 0
     for result in results:
         x, y = result
         graph[x][y] = 1
@@ -13,10 +15,13 @@ def solution(n, results):
             for y in range(1, n+1):
                 if graph[x][k] == 1 and graph[k][y] == 1:
                     graph[x][y] = 1
-                if graph[x][k] == 0 and graph[k][y] == 0:
+                    graph[y][x] = 0
+                elif graph[x][k] == 0 and graph[k][y] == 0:
                     graph[x][y] = 0
-    for index in range(1, len(graph)):
-        graph[index] = graph[index][1:]
-        if -1 not in graph[index]:
+                    graph[y][x] = 1
+    for index in range(1, n+1):
+        check = graph[index][1:]
+        if -2 not in check:
             answer += 1
+
     return answer
