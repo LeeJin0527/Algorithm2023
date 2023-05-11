e133adfd28de29085aef6df1e205e73452f151c4
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-11 22:04:47 +0900

    [level 4] Title: 도둑질, Time: 612.69 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/lv4/42897. 도둑질/도둑질.py
+++ 프로그래머스/lv4/42897. 도둑질/도둑질.py
@@ -1,20 +1,16 @@
 def solution(money):
     answer = 0
-    first = [0] + money[1:]
-    second = [0] + money[:-1]
-    
-    firstDP = [0] * len(money)
-    secondDP = [0] * len(money)
-    
-    firstDP[0] = first[0]
-    firstDP[1] = max(first[0], first[1])
-    
-    secondDP[0] = second[0]
-    secondDP[1] = max(second[0], second[1])
-    
-    for index in range(2, len(first)):
-        firstDP[index] = max(firstDP[index-1], firstDP[index-2] + first[index])
-        secondDP[index] = max(secondDP[index-1], secondDP[index-2] + second[index])
-    
-    
-    return max(firstDP[-1], secondDP[-1])
+    money1 = [0] + money[:-1]
+    money2 = [0] + money[1:]
+    dp1 = [0 for _ in range(len(money))] 
+    dp2 = [0 for _ in range(len(money))] 
+    dp1[0] = money1[0]
+    dp1[1] = max(money1[0], money1[1])
+    dp2[0] = money2[0]
+    dp2[1] = max(money2[0], money2[1])
+    for index in range(2, len(money1)):
+        dp1[index] = max(dp1[index-2] + money1[index], dp1[index-1])
+    for index in range(2, len(money2)):
+        dp2[index] = max(dp2[index-2] + money2[index], dp2[index-1])
+
+    return max(dp1[-1], dp2[-1])
