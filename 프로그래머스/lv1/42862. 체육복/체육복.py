46ba998a8a6b15ec15a99235aea496c8571d481b
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-26 22:07:23 +0900

    [level 1] Title: 체육복, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,27 +1,14 @@
-import copy
 def solution(n, lost, reserve):
-    lost.sort()
-    reserve.sort()
-    origin = len(lost)
-    answer = 0
-    check = []
-    for lostPerson in lost:
-        if lostPerson in reserve:
-            check.append(lostPerson)
-    for che in check:
-        reserve.remove(che)
-        lost.remove(che)
-    compareLost = copy.deepcopy(lost)
-    for lostPerson in compareLost:
-        if lostPerson-1 in reserve:
-            if lostPerson-1 not in check:
-                check.append(lostPerson-1)
-                reserve.remove(lostPerson-1)
-                lost.remove(lostPerson)
-                
-        elif lostPerson+1 in reserve:
-            if lostPerson+1 not in check:
-                check.append(lostPerson+1)
-                reserve.remove(lostPerson+1)
-                lost.remove(lostPerson)
-    return n - origin + len(check)
+
+    n_lost = set(lost) - set(reserve)
+    n_reserve = set(reserve) - set(lost)
+
+    for x in n_lost:
+        if x-1 in n_reserve:
+            n_reserve.remove(x-1)
+        elif x+1 in n_reserve:
+            n_reserve.remove(x+1)
+        else:
+            n -= 1
+        
+    return n
