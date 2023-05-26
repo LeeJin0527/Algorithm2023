ab486873844aa9f8ea6f597b19b84b242a08a3ea
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-26 21:44:23 +0900

    [level 2] Title: 구명보트, Time: 12.64 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,26 +1,17 @@
 from collections import deque
 def solution(people, limit):
     answer = 0
-    if len(people) == 1:
-        return 1
     people.sort(reverse = True)
     people = deque(people)
-    
     while people:
-        if len(people) > 2:
+        if len(people) >= 2:
             if people[0] + people[-1] <= limit:
-                people.popleft()
                 people.pop()
-                answer += 1
+                people.popleft()
             else:
                 people.popleft()
-                answer += 1
         else:
-            if people[0] + people[-1] <= limit and len(people) == 2:
-                people.popleft()
-                people.pop()
-                answer += 1
-            else:
-                answer += len(people)
-                break
+            people.popleft()
+        answer += 1
+        
     return answer
