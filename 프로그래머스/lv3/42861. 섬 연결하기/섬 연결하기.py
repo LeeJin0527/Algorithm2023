7e0e17dc2b82f7717a3d9b32ac96673906ca863e
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-26 21:59:30 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,26 +1,15 @@
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
-    
-def union_find(parent, x, y):
-    x = find_parent(parent, x)
-    y = find_parent(parent, y)
-    if x < y:
-        parent[x] = y
-    else:
-        parent[y] = x
-        
 def solution(n, costs):
     answer = 0
-    parent = [0 for _ in range(n+1)]
-    for i in range(1, n+1):
-        parent[i] = i
     costs.sort(key=lambda x: x[2])
-    for cost in costs:
-        x, y, c = cost
-        if find_parent(parent, x) != find_parent(parent, y):
-            union_find(parent, x, y)
-            answer += c
+    connect = set()
+    connect.add(costs[0][0])
 
+    while len(connect) != n:
+        for cost in costs:
+            if (cost[0] in connect) and (cost[1] in connect):
+                continue
+            if (cost[0] in connect) or (cost[1] in connect):
+                connect.update([cost[0], cost[1]])
+                answer += cost[2]
+                break
     return answer
