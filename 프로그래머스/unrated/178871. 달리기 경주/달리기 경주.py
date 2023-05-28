2b8975d11f08dabb6e8cc13934be455a4fe29969
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-29 00:53:15 +0900

    [unrated] Title: 달리기 경주, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -1,22 +1,17 @@
 from collections import defaultdict
 def solution(players, callings):
     answer = []
-    # 플레이어의 랭킹
     rank = defaultdict(int)
-    # 플레이어의 위치
     location = defaultdict(int)
     for index, value in enumerate(players):
         rank[value] = index
         location[index] = value
- 
+    
     for index, value in enumerate(callings):
-        backIndex = rank[value]
-        frontIndex = backIndex - 1
-        location[backIndex], location[frontIndex] = location[frontIndex] , location[backIndex]
-
-        rank[location[backIndex]], rank[location[frontIndex]] = rank[location[frontIndex]], rank[location[backIndex]]
-        
-        
+        curIndex = rank[value]
+        preIndex = curIndex -1
+        location[curIndex], location[preIndex] = location[preIndex], location[curIndex]
+        rank[location[curIndex]], rank[location[preIndex]] = rank[location[preIndex]], rank[location[curIndex]]
     for loc in location:
         answer.append(location[loc])
     return answer
