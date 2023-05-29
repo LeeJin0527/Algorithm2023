173a3b63c1c4b678af6b9887bbda85f17d83ad41
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-29 12:31:03 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -9,7 +9,7 @@ def solution(players, callings):
     
     for index, value in enumerate(callings):
         curIndex = rank[value]
-        preIndex = curIndex -1
+        preIndex = curIndex-1
         location[curIndex], location[preIndex] = location[preIndex], location[curIndex]
         rank[location[curIndex]], rank[location[preIndex]] = rank[location[preIndex]], rank[location[curIndex]]
     for loc in location:
