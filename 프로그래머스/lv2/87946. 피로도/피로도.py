77a0a2819c798eda5dcbd7b4457346fa9ac430cf
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-05-31 20:53:19 +0900

    [level 2] Title: 피로도, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,21 @@
+visited = []
+n = 0
+answer = 0
+def dfs(k, cnt, dungeons):
+    global answer
+    if cnt > answer:
+        answer = cnt
+        
+    for j in range(n):
+        if k >= dungeons[j][0] and not visited[j]:
+            visited[j] = 1
+            dfs(k - dungeons[j][1], cnt + 1, dungeons)
+            visited[j] = 0
+def solution(k, dungeons):
+    global n
+    global visited
+    n = len(dungeons)
+    visited = [0] * n
+    dfs(k, 0, dungeons)
+    
+    return answer
