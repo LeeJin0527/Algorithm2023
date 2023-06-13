4fb94b562e8bd6a803ddc497e2d0b19bb8ed0cbb
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-06-13 13:46:46 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -0,0 +1,17 @@
+def solution(a, b, n):
+    answer = 0
+    t = 0
+    while True:
+        share = int(n / a)
+        answer += b * share
+        remainder = n % a
+        
+        n = b * share
+        n += remainder
+        if n < a:
+            break
+        if n == a:
+            answer += 1
+            break
+
+    return answer
