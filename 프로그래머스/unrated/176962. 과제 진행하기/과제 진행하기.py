6add967ce6174f5013e179c75a8854859ab29d3a
JIN <64263694+LeeJin0527@users.noreply.github.com>
2023-06-26 13:46:01 +0900

    [unrated] Title: 과제 진행하기, Time: 1.74 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
@@ -1,32 +1,27 @@
-from collections import deque
 def solution(plans):
-    answer = []
-    stack = []
+    answer, check, stack = [], [], []
     plans.sort(key=lambda x: x[1])
-    check = []
     for plan in plans:
         subject, start, duration = plan
         hour, minute = start.split(":")
-        total = 60 * int(hour) + int(minute)
-        check.append([subject, total, int(duration)])
-    
+        check.append([subject, 60 * int(hour) + int(minute), int(duration)])
+    leftTime = 0
     for i in range(len(check)):
         subject, start, duration = check[i]
         while stack:
-            prevSubject, prevTime = stack.pop()
-            if leftTime >= prevTime:
-                leftTime -= prevTime
+            prevSubject, prevDuration = stack.pop()
+            if leftTime >= prevDuration:
+                leftTime -= prevDuration
                 answer.append(prevSubject)
             else:
-                stack.append([prevSubject, prevTime - leftTime])
+                stack.append([prevSubject, prevDuration - leftTime])
                 break
-            
-            
         stack.append([subject, duration])
         if i < len(check)-1:
             nextStart = check[i+1][1]
             leftTime = nextStart - start
+    
     while stack:
         ans, _ = stack.pop()
         answer.append(ans)
-    return answer 
+    return answer
