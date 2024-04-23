aa9834d369a62711468a61620cceb237a0b36ec7
JIN <64263694+LeeJin0527@users.noreply.github.com>
2024-04-23 23:14:38 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 3.47 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = Arrays.stream(num_list)
+            .sorted()
+            .limit(5)
+            .toArray();
+        return answer;
+    }
+}
