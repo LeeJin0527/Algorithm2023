c160565a9d39d825aa55eeb1b03a1ffbd566d911
JIN <64263694+LeeJin0527@users.noreply.github.com>
2024-04-23 23:12:17 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 3.19 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,13 @@
+import java.util.Arrays; 
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        
+        int[] answer = Arrays.stream(num_list)
+            .sorted()
+            .skip(5)
+            .toArray();
+        
+        return answer;
+    }
+}
